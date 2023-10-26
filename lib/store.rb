1941d5d9348d50791adb5bc919e0e3937d1e8369
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-10-26 09:50:13 -0400

    add validations for employee and store

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_apparel, "must carry at least one type of apparel (men's or women's)")
+      errors.add(:womens_apparel, "must carry at least one type of apparel (men's or women's)")
+    end
+  end
 end
