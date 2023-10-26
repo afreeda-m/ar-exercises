7c44e82ee916761f15eab964f2b0a18e6424d376
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-10-26 09:23:51 -0400

    calculate revenue for store

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for the entire company: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue for all stores: $#{average_revenue}"
+
+high_revenue_stores = Store.where("annual_revenue >= ?", 1000000)
+num_high_revenue_stores = high_revenue_stores.size
+puts "Number of stores generating $1M or more in annual sales: #{num_high_revenue_stores}"
